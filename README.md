733ea6b2e55d7f84bf8f0ccefc55952935fddcce
ahmedbani <a13banisal@gmail.com>
2021-10-20 19:38:24 +0300

    lab 8

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # quotes
+
+in this application we have a Json file that contains data about quotes : 
+
+- tags: what the quote is about  
+- author: the one who wrote that quote  
+- likes: how many like the quote got  
+- text: the quote it self  
+
+every time you run the program it will give you the quote it self and the author who wrote the quote randomly like : 
+
+`{"author":"Marilyn Monroe","text": “I am good, but not an angel. I do sin, but I am not the devil. I am just a small girl in a big world trying to find someone to love.” }`
---
+++ app/src/main/java/quotes/Quotes.java
@@ -0,0 +1,40 @@
+package quotes;
+
+import java.util.ArrayList;
+
+public class Quotes {
+    private String author;
+    private String text;
+
+    public Quotes(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    public Quotes() {
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"author\":\"" + author + '\"' +
+                ",\"text\":" + text +
+                "}";
+    }
+}
